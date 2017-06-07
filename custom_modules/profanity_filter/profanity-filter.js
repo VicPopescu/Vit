1034bd57065a4ca398be470e2b167b56354126f8
Victor Popescu <nomevictor@gmail.com>
2017-06-07 18:42:24 +0300

    added custom profanity filter

---
+++ custom_modules/profanity_filter/profanity-filter.js
@@ -0,0 +1,119 @@
+/**
+ *      Profanity Filter Module
+ */
+var ProfanityFilter = (function () {
+
+    /**
+     *      Dependencies
+     */
+    var fs = require('fs');
+
+
+    /**
+     *      Global variables
+     */
+    var dictionary_location = __dirname + '/bad_words_dictionary/dictionary.JSON';
+
+    /**
+     *      Return entire dictionary
+     */
+    var getDictionaryJSON = function () {
+
+        function fsCallback (err, dictionary) {
+
+            //check for config reading errors
+            if (err) {
+                return console.log("Read dictionary file error: ", err);
+            }
+
+            return dictionary;
+        }
+
+        return JSON.parse(fs.readFileSync(dictionary_location, 'utf8', fsCallback));
+    }
+
+
+    /**
+     *      Read dictionary file and return regex with bad words
+     */
+    var badWords = function () {
+
+        return new RegExp(getDictionaryJSON().badWords.join("|"), 'gi');
+    }
+
+
+    /**
+     *      Read dictionary file and return replacement words
+     */
+    var goodWord = function () {
+
+        var arr = getDictionaryJSON().fluffyWords;
+
+        return arr[Math.floor(Math.random() * arr.length)];
+    }
+
+    /**
+     *      Add new bad words to dictionary
+     */
+    var addBadWord = function (newBadWord) {
+
+        fs.readFile(dictionary_location, 'utf8', function (err, dictionary) {
+
+            if (err) return console.log(err);
+
+            //parse external JSON into object that can be modified
+            var parsedDictionary = JSON.parse(dictionary);
+
+            //add bad word if it's not already added
+            if (parsedDictionary.badWords.indexOf(newBadWord) == -1) {
+
+                parsedDictionary.badWords.push(newBadWord);
+
+                //stringify object so it can be writted on external JSON file
+                parsedDictionary = JSON.stringify(parsedDictionary);
+
+                fs.writeFile(dictionary_location, parsedDictionary, 'utf8', function (err) {
+
+                    if (err) return console.log(err);
+                    console.log("Successful writed!")
+                });
+            } else {
+                console.log("That bad word already exists!");
+            }
+        });
+    }
+
+    /**
+     *      Replace bad words with fluffy unicorns
+     */
+
+    var filterReplace = function (message) {
+
+        var good = goodWord();
+
+        var bad = badWords();
+
+            return message.replace(bad, good);
+    }
+
+    /**
+     *      Public exports
+     */
+    var PUBLIC_METHODS = {
+
+        getDictionary: getDictionaryJSON,
+
+        getBadWords: badWords,
+
+        getGoodWord: goodWord,
+
+        addBadWord: addBadWord,
+
+        filterReplace: filterReplace
+    };
+
+    return PUBLIC_METHODS;
+
+})();
+
+module.exports = ProfanityFilter;
--- server/index.js
+++ server/index.js
@@ -2,16 +2,38 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-
 const path = require('path');
 const fs = require('fs');
+//
+const root = __dirname;
 
-app.use(express.static(path.join(__dirname, '../public')));
-
-server.listen(process.env.port || 4000, function () {
-    console.log('listening on *:4000');
-});
-
+/**
+ *      Custom modules
+ */
+//profanity filter
+const profanityFilter = require('../custom_modules/profanity_filter/profanity-filter.js');
+//set static public folder
+app.use(express.static(path.join(root, '../public')));
+//test connection
 io.on('connection', function (client) {
     client.emit('testConnection');
 });
+
+
+/**
+ *      Server management functions
+ */
+
+//handling events for starting server
+const startServer = function () {
+
+    console.log("\n(SERVER): Vit Server started...");
+    console.log("(SERVER): PID " + process.pid);
+};
+
+/**
+ *      Server management binds
+ */
+
+//start server
+server.listen(process.env.port || 4000, startServer);
