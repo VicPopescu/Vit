72fdb96166581bbdd0cbc794e2f43f7e68f41a38
Victor Popescu <nomevictor@gmail.com>
2017-06-08 03:30:19 +0300

    Basic client-server communication

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,27 @@
 const address = 'http://localhost:4000';
 const socket = io.connect(address);
 
-socket.on('testConnection', function () {
-    alert("Connected");
+var template_message = function (m) {
+    m = '<li>' + m + '</li>';
+    return m;
+};
+
+socket.on('connection', function () {
+    //we need potato connection here
+});
+
+socket.on('new message', function (msg) {
+    $('#chat__allMessages').append(template_message(msg));
+});
+
+$('#chat__form').submit(function (e) {
+
+    var message = $('#chat__userInput').val();
+
+    socket.emit('chat message', message);
+
+    $('#chat__userInput').val('');
+
+    e.preventDefault();
+    return false;
 });
--- server/index.js
+++ server/index.js
@@ -11,20 +11,54 @@ const sassMiddleware = require('node-sass-middleware');
  */
 //profanity filter
 const profanityFilter = require('../custom_modules/profanity_filter/profanity-filter.js');
+
+/**
+ *      Routes
+ */
 //sass middleware
 app.use(sassMiddleware({
     /* Options */
     src: path.join(__dirname, 'sass'),
     dest: path.join(__dirname, '../public/css'),
-    debug: true,
+    debug: false,
     outputStyle: 'compressed',
-    prefix:  '/css'
+    prefix: '/css'
 }));
 //set static public folder
 app.use(express.static(path.join(__dirname, '../public')));
+
+
+// // sending to sender-client only
+// socket.emit('message', "test message");
+
+// // sending to all clients, include sender
+// io.emit('message', "another test message");
+
+// // sending to all clients except sender
+// socket.broadcast.emit('message', "this realy is a test");
+
+// // sending to all clients in 'game' room(channel) except sender
+// socket.broadcast.to('game').emit('message', 'ubabuba');
+
+// // sending to all clients in 'game' room(channel), include sender
+// io.in('game').emit('message', 'amazing potato');
+
+// // sending to sender client, only if they are in 'game' room(channel)
+// socket.to('game').emit('message', 'ghita FTW');
+
+// // sending to all clients in namespace 'myNamespace', include sender
+// io.of('myNamespace').emit('message', 'painful potato');
+
+// // sending to individual socketid
+// socket.broadcast.to(socketid).emit('message', 'now you see me!');
+
+
 //test connection
 io.on('connection', function (client) {
-    client.emit('testConnection');
+    client.on('chat message', function (msg) {
+        console.log('(ANY CLIENT): ' + msg);
+        io.emit('new message', msg);
+    });
 });
 
 
@@ -37,9 +71,10 @@ var serverStatus = function () {
 
     var port = server.address().port;
 
-    console.log("\n(SERVER): Vit Server started...");
-    console.log("(SERVER): PID " + process.pid);
-    console.log("(SERVER): PORT " + port);
+    console.log("\n(SERVER INFO): Vit started...");
+    console.log("(SERVER INFO): PID " + process.pid);
+    console.log("(SERVER INFO): PORT " + port);
+    console.log("\n");
 };
 
 /**
